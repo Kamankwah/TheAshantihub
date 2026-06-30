9e206bf3509a96b82183db6d4d2c93176a170a10
Kamankwah <Addo.kwabena26@gmail.com>
2026-06-30 04:17:23 -0400

    public

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "ashantihub-v1";
+const OFFLINE_URL = "/";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) =>
+      cache.addAll(["/", "/src/main.jsx", "/favicon.svg", "/manifest.json"])
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request).catch(() =>
+        caches.match(OFFLINE_URL)
+      )
+    );
+  }
+});
